add15b0edfaa2a3cd9ee5bb634de8dcaf3b2df31
Jon Mooney <jon.mooney@yahoo.com>
2022-04-20 13:37:29 -0700

    added comments and getter

--- server.js
+++ server.js
@@ -13,9 +13,12 @@ app.use(require('./routes'));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+})
+.then(() => {
+  app.listen(PORT, () => console.log(`🌍 Connected on port:${PORT}`));
 });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
